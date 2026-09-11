d6ff7d5f1f56132caa996db7046c92c975c9b16f
DitisKees <45658586+DitisKees@users.noreply.github.com>
2026-09-11 08:17:31 +0200

    Fix Jest transform for PocketBase ESM tests

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@react-navigation/.*|react-navigation|@tanstack/.*)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@react-navigation/.*|react-navigation|@tanstack/.*|pocketbase)',
   ],
 };
